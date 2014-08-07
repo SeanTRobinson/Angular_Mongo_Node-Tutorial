1bfe6aed12571d77c1b4dc50663f5d6a77777a1c
Sean Robinson <Sean@SeanTRobinson.co.uk>
2014-08-07 11:28:29 +0100

    Added HTTP examples

---
+++ Samples/HTTP/http_client_get.js
@@ -0,0 +1,22 @@
+var http = require('http');
+var options = {
+    hostname: 'localhost',
+    port: '8080'
+};
+
+function handleResponse(response) {
+    var serverData = '';
+    response.on('data', function(chunk) {
+        serverData += chunk;
+    });
+    response.on('end', function() {
+        console.log("Reponse Status: ", response.statusCode);
+        console.log("Response Headers: ", response.headers);
+        console.log(serverData);
+    });
+}
+
+http.request(options, function(response) {
+    handleResponse(response);
+}).end();
+
