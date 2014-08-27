4f90e5b102af4e359e6823913379bec70537b280
Sean Robinson <Sean.Robinson@LexAble.com>
2014-08-27 09:52:25 +0100

    Added demonstrations of express middleware

---
+++ Samples/Express-Middleware/express_cookies.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var cookieParser = require('cookie-parser');
+var app = express();
+
+app.use(cookieParser());
+
+app.get('/', function(req, res) {
+    console.log(req.cookies);
+    if (!req.cookies.hasVisited) {
+        res.cookie('hasVisited', '1',
+            { 
+                maxAge: 60*60*1000,
+                httpOnly: true,
+                path: '/'
+            });
+    }
+    res.send("Sending cookie");
+});
+
+app.listen(80);
