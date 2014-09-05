0d522b4d2c39e69ee8cf36bb9bb38277ff936a0a
Sean Robinson <Sean@SeanTRobinson.co.uk>
2014-09-05 15:23:35 +0100

    Added AngularJS filtering examples

---
+++ Samples/Angular/js/angular_filters.js
@@ -0,0 +1,6 @@
+angular.module('myApp', []).
+    controller('myController', function($scope) {
+        $scope.JSONObj = {title: 'myTitle'};
+        $scope.word = 'Supercalifragilisticexpialidocious';
+        $scope.days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+    });
